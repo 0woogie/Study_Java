a1c8b902e2ab7fb8663cfc69dd6422d30a45576b
0woogie <80441481+0woogie@users.noreply.github.com>
2021-08-22 03:11:43 +0900

    Add files via upload

---
+++ Chapter5/c05p02.java
@@ -0,0 +1,40 @@
+package c05;
+//5장 2번
+//ColorTV를 상속받은 IPTV 클래스 작성하기
+
+class TV2 {
+	private int size;
+	public TV2(int size) { this.size = size; }
+	protected int getSize() { return size; }
+}
+
+class ColorTV2 extends TV2 {
+	private int color;
+	public ColorTV2(int size, int color) {
+		super(size);
+		this.color = color;
+	}
+	void printProperty() {
+		System.out.println(getSize() + "인치 " + color + "컬러");
+	}
+}
+
+class IPTV extends ColorTV2 {
+	private String address;
+	public IPTV(String address, int size, int color) {
+		super(size, color);
+		this.address = address;
+	}
+	@Override
+	void printProperty() {
+		System.out.print("나의 IPTV는 " + address + " 주소의 ");
+		super.printProperty();
+	}
+}
+
+public class c05p02 {
+	public static void main(String[] args) {
+		IPTV iptv = new IPTV("192.1.1.2", 32, 2048);
+		iptv.printProperty();
+	}
+}
