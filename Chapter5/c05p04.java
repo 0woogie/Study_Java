ef6ed2a1e8182a324a39c7f862cbd5d76cb623c2
0woogie <80441481+0woogie@users.noreply.github.com>
2021-08-23 21:37:12 +0900

    Add files via upload

---
+++ Chapter5/c05p04.java
@@ -0,0 +1,43 @@
+package c05;
+//5장 4번
+//Converter 클래스를 상속받아 km를 mile(마일)로 변환하는 Km2Mile 클래스를 작성하기
+import java.util.Scanner;
+
+abstract class Converter2 {
+	abstract protected double convert(double src);
+	abstract protected String getSrcString();
+	abstract protected String getDestString();
+	protected double ratio;
+	
+	public void run() {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println(getSrcString() + "을 " + getDestString() + "로 바꿉니다.");
+		System.out.println(getSrcString() + "을 입력하세요>> ");
+		double val = scanner.nextDouble();
+		double res = convert(val);
+		System.out.println("변환 결과: " + res + getDestString() + "입니다");
+		scanner.close();
+	}
+}
+
+class Km2Mile extends Converter2 {
+	Km2Mile(double ratio){
+		this.ratio = ratio;
+	}
+	protected double convert(double src) {
+		return src/ratio;
+	}
+	protected String getSrcString() {
+		return "Km";
+	}
+	protected String getDestString() {
+		return "mile";
+	}
+}
+
+public class c05p04 {
+	public static void main(String[] args) {
+		Km2Mile toMile = new Km2Mile(1.6);
+		toMile.run();
+	}
+}
