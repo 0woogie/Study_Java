1585bfe1466efcc2c53c450f5abf44d01c850b09
0woogie <80441481+0woogie@users.noreply.github.com>
2021-08-24 22:33:41 +0900

    Add files via upload

---
+++ Chapter5/c05p05.java
@@ -0,0 +1,38 @@
+package c05;
+//5장 5번
+//Point를 상속받아 색을 가진 점을 나타내는 ColorPoint 클래스 작성하기
+
+class Point {
+	private int x, y;
+	public Point(int x, int y) { this.x = x; this.y = y; }
+	public int getX() { return x; }
+	public int getY() { return y; }
+	protected void move(int x, int y) {this.x = x; this.y = y;}
+}
+
+class ColorPoint extends Point {
+	private String color;
+	public ColorPoint(int x, int y, String color) {
+		super(x, y);
+		this.color = color;
+	}
+	public void setXY(int x, int y) {
+		move(x, y);
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public String toString() {
+		return color+"색의 (" + getX() + "," + getY() + ")의 점";
+	}
+}
+
+public class c05p05 {
+	public static void main(String[] args) {
+		ColorPoint cp = new ColorPoint(5, 5, "YELLOW");
+		cp.setXY(10, 20);
+		cp.setColor("RED");
+		String str = cp.toString();
+		System.out.println(str + "입니다.");
+	}
+}
