b319bb7e20ec604465a9018929536069713d4a2f
0woogie <80441481+0woogie@users.noreply.github.com>
2021-08-25 00:20:26 +0900

    Update c05p06.java

--- Chapter5/c05p06.java
+++ Chapter5/c05p06.java
@@ -2,25 +2,25 @@ package c05;
 //5장 6번
 //Point를 상속받아 색을 가진 점을 나타내는 ColorPoint 클래스 작성하기
 
-class Point2 {
+class Point {
 	private int x, y;
-	public Point2(int x, int y) { this.x = x; this.y = y; }
+	public Point(int x, int y) { this.x = x; this.y = y; }
 	public int getX() { return x; }
 	public int getY() { return y; }
 	protected void move(int x, int y) {this.x = x; this.y = y;}
 }
 
-class ColorPoint2 extends Point2 {
+class ColorPoint extends Point {
 	private String color;
-	public ColorPoint2() {
+	public ColorPoint() {
 		super(0, 0);
 		this.color = "BLACK";
 	}
-	public ColorPoint2(int x, int y) {
+	public ColorPoint(int x, int y) {
 		super(x, y);
 		this.color = "BLACK";
 	}
-	public ColorPoint2(int x, int y, String color) {
+	public ColorPoint(int x, int y, String color) {
 		super(x, y);
 		this.color = color;
 	}
@@ -37,10 +37,10 @@ class ColorPoint2 extends Point2 {
 
 public class c05p06 {
 	public static void main(String[] args) {
-		ColorPoint2 zeroPoint = new ColorPoint2();
+		ColorPoint zeroPoint = new ColorPoint();
 		System.out.println(zeroPoint.toString() + "입니다.");
 		
-		ColorPoint2 cp = new ColorPoint2(10, 10);
+		ColorPoint cp = new ColorPoint(10, 10);
 		cp.setXY(5, 5);
 		cp.setColor("RED");
 		System.out.println(cp.toString() + "입니다.");
