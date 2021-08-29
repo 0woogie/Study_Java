9fbf81e3e3116fcae7c63ca4512475811925a2ad
0woogie <80441481+0woogie@users.noreply.github.com>
2021-08-29 16:36:52 +0900

    Update c05p14.java

--- Chapter5/c05p14.java
+++ Chapter5/c05p14.java
@@ -2,7 +2,7 @@ package c05;
 //5장 14번
 //문제 13번의 Shape 인터페이스를 구현하는 클래스 Oval, Rect를 추가 작성하여 전체 프로그램 완성하기
 
-interface Shape2 {
+interface Shape {
 	final double PI = 3.14;
 	void draw();
 	double getArea();
@@ -12,9 +12,9 @@ interface Shape2 {
 	}
 }
 
-class Circle2 implements Shape2 {
+class Circle implements Shape {
 	private int r;
-	public Circle2(int r) {
+	public Circle(int r) {
 		this.r = r;
 	}
 	public void draw() {
@@ -25,9 +25,9 @@ class Circle2 implements Shape2 {
 	}
 }
 
-class Oval2 implements Shape2 {
+class Oval implements Shape {
 	private int x, y;
-	public Oval2(int x, int y) {
+	public Oval(int x, int y) {
 		this.x = x;
 		this.y = y;
 	}
@@ -39,9 +39,9 @@ class Oval2 implements Shape2 {
 	}
 }
 
-class Rect2 implements Shape2 {
+class Rect implements Shape {
 	private int x, y;
-	public Rect2(int x, int y) {
+	public Rect(int x, int y) {
 		this.x = x;
 		this.y = y;
 	}
@@ -55,10 +55,10 @@ class Rect2 implements Shape2 {
 
 public class c05p14 {
 	public static void main(String[] args) {
-		Shape2[] list = new Shape2[3];
-		list[0] = new Circle2(10);
-		list[1] = new Oval2(20, 30);
-		list[2] = new Rect2(10, 40);
+		Shape[] list = new Shape[3];
+		list[0] = new Circle(10);
+		list[1] = new Oval(20, 30);
+		list[2] = new Rect(10, 40);
 		
 		for(int i=0; i<list.length; i++) list[i].redraw();
 		for(int i=0; i<list.length; i++) System.out.println("면적은 " + list[i].getArea());
