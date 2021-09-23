8c654209eee6dc7c0241afa7d6d632958f1e08cf
0woogie <80441481+0woogie@users.noreply.github.com>
2021-09-23 21:00:23 +0900

    Add files via upload

---
+++ Chapter7/c07p09.java
@@ -0,0 +1,41 @@
+package c07;
+//7장 9번
+//IStack<T> 인터페이스를 구현하는 MyStack<T> 클래스 작성하기
+import java.util.Vector;
+
+interface IStack<T> {
+	T pop();
+	boolean push(T ob);
+}
+
+class MyStack<T> implements IStack<T> {
+	Vector<T> v = null;
+	MyStack() {
+		v = new Vector<T>();
+	}
+	@Override
+	public T pop() {
+		if(v.size() == 0)
+			return null;
+		else
+			return v.remove(0);
+	}
+	@Override
+	public boolean push(T ob) {
+		v.add(0, ob);
+		return true;
+	}
+	
+}
+
+public class c07p09 {
+	public static void main(String[] args) {
+		IStack<Integer> stack = new MyStack<Integer>();
+		for(int i=0; i<10; i++) stack.push(i);
+		while(true) {
+			Integer n = stack.pop();
+			if(n == null) break;
+			System.out.print(n + " ");
+		}
+	}
+}
